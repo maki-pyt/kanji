996756a0cb8e88222d755adec4853d84be931acb
maki-pyt <hayato.aya.0831@gmail.com>
2024-07-06 17:09:44 +0900

    Update kanjiapp2.py

--- kanjiapp2.py
+++ kanjiapp2.py
@@ -71,9 +71,11 @@ if page == '7級':
             C1=sheet3.cell(row=i,column=j) #sheet1のセルの行番号と列番号を指定している
             C2=sheet4.cell(row=j,column=i) #sheet1のセルの行番号と列番号を入れ替えてsheet2のセルを指定している
             C2.value=C1.value #sheet2のセルにsheet1のセルの値を代入
-    Path("G:/文書").mkdir(exist_ok=True)
+    code = "print(os.getcwd())"
+    st.code(code)
+    #Path("G:/文書").mkdir(exist_ok=True)
      # 保存先を指定
-    wb.save("G:/文書/kanjiprint.xlsx")
+    #wb.save("G:/文書/kanjiprint.xlsx")
     
     if st.button('プリント作成'):
         st.write('完成しました。以下のURLから印刷して使用してください。')
